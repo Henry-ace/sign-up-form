2d8f6ec4494582cdc28c521421ea34e9ca631a12
Henry-ace <honoja49@gmail.com>
2024-08-16 22:42:02 +0100

    add functionality to the submit button.

--- index.js
+++ index.js
@@ -6,8 +6,16 @@
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password')
 const submitBtn = document.querySelector('#submit-btn');
+const error = document.querySelector('.error')
 
 
-
-
-
+submitBtn.addEventListener('click', () => {
+   if(password.value !== confirmPassword.value) {
+      error.innerHTML = '*Password do not match'
+      error.style.color = 'red'
+      error.style.fontSize = '.7rem';
+      password.style.outline = '1px solid red';
+      confirmPassword.style.outline = '1px solid red';
+      return
+   }
+})
