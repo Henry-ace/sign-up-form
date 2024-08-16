b83880bd62cf0834f7eb676a82b1e57ef5b79fe6
Henry-ace <honoja49@gmail.com>
2024-08-16 15:09:24 +0100

    finished the desktop design

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password')
+const submitBtn = document.querySelector('#submit-btn');
+
+
+
+
+
